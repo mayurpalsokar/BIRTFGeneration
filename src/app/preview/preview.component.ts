8e171a4e191e03b5cf8d32a4aeb45595d4239438
mayurpalsokar <41935029+mayurpalsokar@users.noreply.github.com>
2018-08-16 11:46:42 +0530

    Modified Preview component

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -27,4 +27,19 @@ back() {
 
   this.router.navigate(['create']);
 }
+
+//Image Upload
+localUrl: any[];
+
+showPreviewImage(event: any) {
+  if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.onload = (event: any) => {
+          this.localUrl = event.target.result;
+      }
+      reader.readAsDataURL(event.target.files[0]);
+  }
+}
+
+
 }
